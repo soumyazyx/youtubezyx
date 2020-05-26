0d01e18293afdbfb87395190c3e3f926d3be6dbd
Soumya Ranjan Das <soumyazyx@gmail.com>
2020-05-26 21:23:37 +0530

    .

--- api/views.py
+++ api/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render, redirect
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from apiclient.discovery import build
 
 
 def apiOverview(request):
+    urls = []
     api_key = "AIzaSyBfAUjIhI718gwmsKNdqmSrZ3EU9kwwdz4"
     youtube = build("youtube", "v3", developerKey=api_key)
-    req = youtube.search().list(q="avengers", part="snippet", type="video")
+    req = youtube.search().list(
+        part="snippet",
+        eventType="completed",
+        maxResults=50,
+        order="date",
+        q="cartoon",
+        type="video",
+    )
     res = req.execute()
-    print(res)
-    return JsonResponse(res)
+    for item in res["items"]:
+        url = "https://www.youtube.com/watch?v={}".format(item["id"]["videoId"])
+        title = item["snippet"]["title"]
+        print(title)
+        href = "<a href={}>{}</a>".format(url, title)
+        embed = "<iframe width='420' height='345' src='{}'></iframe><br/><br/>".format(
+            url
+        )
+        urls.append(href)
+    print(urls)
+    str1 = "<br/><br/>".join(urls)
+    # return JsonResponse(res)
+    return HttpResponse(str1)
